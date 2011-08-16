b6d79b69cd41fc1d6181a8d85d3d0c3c35882fe6
shunTireur <shun.tireur@gmail.com>
2011-08-16 14:54:06 +0900

    Edited src/main/TournamentSample.java via GitHub

--- src/main/TournamentSample.java
+++ src/main/TournamentSample.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import com.vaadin.ui.Tree;
@@ -15,49 +16,80 @@ public class TournamentSample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		TournamentSample ts = new TournamentSample();
+
 		List<String> playerList = new ArrayList<String>(Arrays.asList(players));
 
 		int itemSize = (playerList.size() - 1) * 2 + 1;
-		System.out.println(itemSize);
-
-		Tree tree = new Tree();
-
-		String[] gameCell = new String[itemSize];
+		Object[] gameCell = new String[itemSize];
 		for (int i = 0; i < gameCell.length; i++) {
 			gameCell[i] = "item" + (i + 1);
-			tree.addItem(gameCell[i]);
+		}
+
+		Tree tree = ts.getTournamentTree(gameCell);
+
+
+		Integer floor = new Integer(0);
+		outputTreeItems(tree, 1, floor);
+
+
+		System.out.println("done.");
+	}
+
+	private static void outputTreeItems(Tree tree, int id, Integer floor) {
+		for (int i = 0; i < floor; i++)
+			System.out.print("->");
+		System.out.println("item" + (id));
+		if (tree.hasChildren("item" + (id))) {
+			Collection<?> children = tree.getChildren("item" + (id));
+			floor++;
+			for (Object child : children) {
+				String str = child.toString();
+				str = str.replace("item", "");
+				outputTreeItems(tree, Integer.parseInt(str), floor);
+			}
+		}
+		floor--;
+	}
+
+	public Tree getTournamentTree(Object[] objs) {
+		Tree tree = new Tree();
+
+		for (int i = 0; i < objs.length; i++) {
+			tree.addItem(objs[i]);
 		}
 
 		int parentId = 0;
 		int iCnt = 1;
 		int n = 1;
-		while (iCnt < gameCell.length) {
-			tree.setParent(gameCell[iCnt++], gameCell[parentId]);
-			if (iCnt >= gameCell.length) break;
-			tree.setParent(gameCell[iCnt++], gameCell[parentId]);
-			if (iCnt >= gameCell.length) break;
+		while (iCnt < objs.length) {
+			tree.setParent(objs[iCnt++], objs[parentId]);
+			if (iCnt >= objs.length) break;
+			tree.setParent(objs[iCnt++], objs[parentId]);
+			if (iCnt >= objs.length) break;
 			parentId++;
 
 			if (4 <= parentId) {
 				parentId = parentId * 2 - 1;
 				for (int i = 4 * n + 1; i < parentId; i += 2) {
-					tree.setParent(gameCell[iCnt++], gameCell[i]);
-					if (iCnt >= gameCell.length) break;
-					tree.setParent(gameCell[iCnt++], gameCell[i]);
-					if (iCnt >= gameCell.length) break;
+					tree.setParent(objs[iCnt++], objs[i]);
+					if (iCnt >= objs.length) break;
+					tree.setParent(objs[iCnt++], objs[i]);
+					if (iCnt >= objs.length) break;
 				}
-				if (iCnt >= gameCell.length) break;
+				if (iCnt >= objs.length) break;
 				for (int i = 4 * n; i < parentId; i += 2) {
-					tree.setParent(gameCell[iCnt++], gameCell[i]);
-					if (iCnt >= gameCell.length) break;
-					tree.setParent(gameCell[iCnt++], gameCell[i]);
-					if (iCnt >= gameCell.length) break;
+					tree.setParent(objs[iCnt++], objs[i]);
+					if (iCnt >= objs.length) break;
+					tree.setParent(objs[iCnt++], objs[i]);
+					if (iCnt >= objs.length) break;
 				}
 				n++;
 			}
 		}
 
-		System.out.println(tree);
+		return tree;
 	}
 
 }
